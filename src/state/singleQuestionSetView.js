82289e6a3f03040e95b0985e9ed09e1ead91456b
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2019-01-19 14:04:19 +0100

    code refactoring

--- src/state/singleQuestionSetView.js
+++ src/state/singleQuestionSetView.js
@@ -1,27 +1,26 @@
 import { database } from '../firebaseConfig'
-import { restoreInitialStateAction } from './createNewQuestionSetView'
+import { restoreInitialStateAction } from './createNewQuestionSetView';
 
 export const deleteQuestionAsyncAction = (id, index) => (dispatch, getState) => {
     database.ref(`/questionSets/${id}/questions/${index}`).remove()
-}
+};
 
 export const addNewQuestionToFirebaseAsyncAction = (id) => (dispatch, getState) => {
-    const questionText = getState().createNewQuestionSetView.questionText
-    const questionType = getState().createNewQuestionSetView.questionType
-    let answers = getState().createNewQuestionSetView.answers
-    let questionSet = getState().questionSetsListView.questionSets.find(questionSet => questionSet.key === id)
-    const timestamp = Date.now()
-    answers = (questionType === 'checkbox' ? answers : null)
+    const questionText = getState().createNewQuestionSetView.questionText;
+    const questionType = getState().createNewQuestionSetView.questionType;
+    let answers = getState().createNewQuestionSetView.answers;
+    const questionSet = getState().questionSetsListView.questionSets.find(questionSet => questionSet.key === id);
+    let newQuestionSet = {}
+    const timestamp = Date.now();
+    answers = (questionType === 'checkbox' ? answers : null);
 
-    if (questionSet.questions) {
-        questionSet.questions.push({ text: questionText, type: questionType, timestamp, answers })
-    } else {
-        questionSet.questions = []
-        questionSet.questions.push({ text: questionText, type: questionType, timestamp, answers })
-    }
+    questionSet.questions ?
+        newQuestionSet.questions = questionSet.questions.concat({ text: questionText, type: questionType, timestamp, answers })
+        : newQuestionSet.questions = [{ text: questionText, type: questionType, timestamp, answers }]
+
     database.ref(`/questionSets/${id}/questions`).set(
-        questionSet.questions
-    )
+        newQuestionSet.questions
+    );
 
-    dispatch(restoreInitialStateAction())
-}
+    dispatch(restoreInitialStateAction());
+};
